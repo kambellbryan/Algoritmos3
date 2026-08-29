8d32883ec4d7663ebf0ab046342443991547e4e9
Brayan <brayan@example.com>
2026-08-29 12:22:22 -0500

    Arrays

--- array.java
+++ array.java
@@ -1,18 +1,24 @@
 import java.util.Scanner;
 
 public class array {
+
     public static void main(String[] args) {
+
         try (Scanner teclado = new Scanner(System.in)) {
-            System.out.print("Ingrese el tamano del arreglo: ");
+
+            System.out.print("Ingrese el tamaño del arreglo: ");
             int tam = teclado.nextInt();
+
             int[] array = new int[tam];
 
             System.out.println("Ingrese los elementos del arreglo:");
+
             for (int i = 0; i < tam; i++) {
                 array[i] = teclado.nextInt();
             }
 
             System.out.println("Los elementos del arreglo son:");
+
             for (int i = 0; i < tam; i++) {
                 System.out.print(array[i] + " ");
             }
